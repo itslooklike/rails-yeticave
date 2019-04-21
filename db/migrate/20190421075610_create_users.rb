463e831c26ad584f13af3c1d6f086e063cf40994
itslooklike <delsol@bk.ru>
2019-04-21 11:28:43 +0300

    migrations

--- db/migrate/20190421075610_create_users.rb
+++ db/migrate/20190421075610_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.date_field :reg_date
+      t.datetime :reg_date
       t.string :email
       t.string :name
       t.string :password
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = [
+  'Доски и лыжи',
+  'Крепления',
+  'Ботинки',
+  'Одежда',
+  'Инструменты',
+  'Разное'
+]
+
+categories.each do |category|
+  Category.create(name: category)
+end
+
+users = [
+  { reg_date: DateTime.now - 10, email: 'ignat.v@gmail.com', name: 'Игнат', password: '123', avatar_url: 'img/ava1.png', contact: 'Скайп' },
+  { reg_date: DateTime.now - 50, email: 'kitty_93@li.ru', name: 'Леночка', password: '123', avatar_url: 'img/ava2.png', contact: 'Гитхаб' },
+  { reg_date: DateTime.now - 100, email: 'warrior07@mail.ru', name: 'Руслан', password: '123', avatar_url: 'img/ava3.png', contact: 'ВК' },
+]
+
+users.each do |user|
+  User.create(user)
+end

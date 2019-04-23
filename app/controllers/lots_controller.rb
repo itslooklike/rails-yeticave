23f340ace35974a65226156eb06c73bfb0cddb34
a.masalov <delsol@bk.ru>
2019-04-23 11:55:03 +0300

    fix upload

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -29,10 +29,17 @@ class LotsController < ApplicationController
 
   def upload
     uploaded_io = params[:lot][:image_url]
-    file_path = Rails.root.join('public', 'uploads', SecureRandom.uuid)
+    file_name = SecureRandom.uuid + '.' + Mime::Type.lookup(uploaded_io.content_type).symbol.to_s
+    file_path = Rails.root.join('public', 'uploads', file_name)
+
+    unless File.exist?(File.dirname(file_path))
+      FileUtils.mkdir_p(File.dirname(file_path))
+    end
+
     File.open(file_path, 'wb') do |file|
       file.write(uploaded_io.read)
     end
-    file_path
+
+    "/uploads/#{file_name}"
   end
 end

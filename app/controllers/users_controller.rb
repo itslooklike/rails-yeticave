4f32672dcb5f6a17d6776f3e8c62c10518012444
itslooklike <delsol@bk.ru>
2019-04-24 00:05:58 +0300

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,53 @@
 class UsersController < ApplicationController
+  before_action :fetch_categories, only: [:new, :create, :login, :sign_in]
+
   def new
-    @categories = Category.all
     @user = User.new
   end
 
   def create
+    @user = User.new user_params
 
+    if User.exists?(email: @user.email)
+      @user.errors.add(:email, "Такой пользователь уже зарегистрирован")
+      render :new
+      return
+    end
+
+    avatar = params[:user][:avatar_url]
+    @user.avatar_url = Uploader.upload(avatar) if avatar
+
+    if @user.save
+      redirect_to action: "login"
+    else
+      render :new
+    end
   end
 
   def login
-    @categories = Category.all
     @user = User.new
   end
 
   def sign_in
+    @user = User.new params.require(:user).permit(:email, :password)
+    current_user = User.where("email = ?", @user.email).first
+
+    if current_user.nil? || current_user.password != @user.password
+      @user.errors.add(:email, "Неверные данные")
+      @user.errors.add(:password, "Неверные данные")
+      render :login
+    else
+      p 'успех'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :avatar_url, :contact)
+  end
+
+  def fetch_categories
     @categories = Category.all
   end
 end

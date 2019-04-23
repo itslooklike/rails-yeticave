abece0957add9499b1d601808a9ebeba4dff7fb0
itslooklike <delsol@bk.ru>
2019-04-23 11:08:33 +0300

    add file upload

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -11,6 +11,7 @@ class LotsController < ApplicationController
 
   def create
     @lot = Lot.new lot_params
+    @lot.update(image_url: upload)
     if @lot.save
       redirect_to @lot
     else
@@ -23,6 +24,15 @@ class LotsController < ApplicationController
   private
 
   def lot_params
-    params.require(:lot).permit(:name, :description, :start_price, :bet_step, :finish_date, :category_id)
+    params.require(:lot).permit(:name, :description, :start_price, :bet_step, :finish_date, :category_id, :image_url)
+  end
+
+  def upload
+    uploaded_io = params[:lot][:image_url]
+    file_path = Rails.root.join('public', 'uploads', SecureRandom.uuid)
+    File.open(file_path, 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    file_path
   end
 end
--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,5 +1,5 @@
 class Lot < ApplicationRecord
-  validates :name, :description, :start_price, :bet_step, :finish_date, :category_id, presence: true
+  validates :name, :description, :start_price, :bet_step, :finish_date, :category_id, :image_url, presence: true
 
   belongs_to :category
 end

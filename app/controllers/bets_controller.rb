cf8dd54dceed9a4c3a840ebd6ecc8c43e3c1d486
itslooklike <delsol@bk.ru>
2019-04-26 23:24:28 +0300

    some bets

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,7 +1,7 @@
 class BetsController < ApplicationController
   def create
     # достать лот ид, и юзера
-    @bet = User.new( bet_params, user_id: @current_user.id)
+    @bet = User.new(bet_params, user_id: @current_user.id)
 
     if @bet.save
       redirect_to @lot
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'lots/:id' => 'lots#show', as: :lot
   post 'lots/new' => 'lots#create'
 
+  post 'bets' => 'bets#create'
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

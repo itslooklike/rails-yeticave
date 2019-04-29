c99e9e66ecd6155c937b6700ccd9fd564ad6b6f4
itslooklike <delsol@bk.ru>
2019-04-30 00:21:51 +0300

    add val

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
 
-    if User.exists?(email: @user.email)
-      @user.errors.add(:email, "Такой пользователь уже зарегистрирован")
-      render :new
-      return
-    end
-
-    avatar = params[:user][:avatar_url]
-    @user.avatar_url = Uploader.upload(avatar) if avatar
-
-    if @user.save
+    if @user.valid?
+      avatar = params[:user][:avatar_url]
+      @user.avatar_url = Uploader.upload(avatar) if avatar
+      @user.save
       # session[:user_id] = @user.id
       # redirect_to '/'
       redirect_to '/login'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bets
   validates :email, :password, presence: true
+  validates :email, uniqueness: true
 
   has_secure_password
 end

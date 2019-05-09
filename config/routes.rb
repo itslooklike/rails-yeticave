57e5b399ef7a5b9d9c1f11e8f7e1e6a0b243241e
itslooklike <delsol@bk.ru>
2019-05-09 12:14:01 +0300

    some session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: :logout
@@ -7,5 +8,4 @@ Rails.application.routes.draw do
   resources :bets, :lots, :users
 
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

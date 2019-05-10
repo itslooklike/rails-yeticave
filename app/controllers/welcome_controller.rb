d9159af503bcb9c68b47b1de9988584203efa5a3
itslooklike <delsol@bk.ru>
2019-05-10 11:44:19 +0300

    add cool find

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @categories = Category.all
-    @lots = @q.result(distinct: true)
+
+    if params[:q]
+      @lots = Lot.where('lower(name) LIKE ?', "%#{params[:q].downcase}%")
+    elsif params[:category]
+      @lots = Lot.where('category_id = ?', params[:category])
+    else
+      @lots = Lot.all
+    end
   end
 end
